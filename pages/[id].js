ace89c68b1376a99f28c9c12a51ee46a2e92698a
pmallol <pmallol@manas.tech>
2020-07-21 16:43:08 -0300

    wip

--- pages/[id].js
+++ pages/[id].js
@@ -1,12 +1,35 @@
+import { useState, useEffect} from 'react'
+import Router from 'next/router';
+
 import Nav from '../components/Nav'
+import Text from '../components/Text'
 import Layout from '../components/Layout'
 
 export default function Garden() {
+  const [garden, setGarden] = useState(null)
+  // useEffect(() => {
+  //   fetch(`https://dry-gardens-api.herokuapp.com/gardens/${Router.query.id}`, {mode: 'cors'})
+  //   .then(res => res.json())
+  //   .then(body => setGarden(body))
+  // })
+
+  if(garden === null) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
   return(
     <>
       <Layout>
         <Nav />
-        <h1>Temple View</h1>
+        <h1>{garden.name}</h1>
+        <Text>{garden.description}</Text>
+        {garden.images.map(i => {
+          return <a href={i.source} target="_blank"> Source</a>
+        })}
       </Layout>
     </>
   )
